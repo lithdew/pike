9f0b2e162f93061b82f1fe3b06d389bbb829f410
lithdew <kenta@lithdew.net>
2020-09-30 19:28:53 +0900

    windows: add GetQueuedCompletionStatusEx and OVERLAPPED_ENTRY

--- src/driver_iocp.zig
+++ src/driver_iocp.zig
@@ -1,16 +1,22 @@
 const std = @import("std");
 const os = std.os;
+const math = std.math;
+const windows = os.windows;
+const ws2_32 = windows.ws2_32;
 
 const pike = @import("pike.zig");
 
 const Self = @This();
 
 executor: pike.Executor,
-handle: os.fd_t = os.windows.INVALID_HANDLE_VALUE,
+handle: os.fd_t = windows.INVALID_HANDLE_VALUE,
+
+const EMPTY_BUFFER: []u8 = &[0]u8{};
+const EMPTY_WSABUF = [1]ws2_32.WSABUF{.{ .len = @intCast(windows.ULONG, EMPTY_BUFFER.len), .buf = EMPTY_BUFFER.ptr }};
 
 pub fn init(opts: pike.DriverOptions) !Self {
-    const handle = try os.windows.CreateIoCompletionPort(os.windows.INVALID_HANDLE_VALUE, null, undefined, std.math.maxInt(os.windows.DWORD));
-    errdefer os.CloseHandle(handle);
+    const handle = try windows.CreateIoCompletionPort(windows.INVALID_HANDLE_VALUE, null, undefined, math.maxInt(windows.DWORD));
+    errdefer os.close(handle);
 
     return Self{ .executor = opts.executor, .handle = handle };
 }
@@ -20,9 +26,29 @@ pub fn deinit(self: *Self) void {
 }
 
 pub fn register(self: *Self, file: *pike.File, comptime event: pike.Event) !void {
-    // TODO(kenta): implement
+    _ = try windows.CreateIoCompletionPort(file.handle, self.handle, @ptrToInt(file), 0);
+
+    if (event.read) {
+        // const rc = ws2_32.WSARecv(@ptrCast(ws2_32.SOCKET, file.handle), EMPTY_WSABUF[0..], @intCast(windows.ULONG, EMPTY_WSABUF.len), null, ws2_32.MSG_PEEK, null, null);
+        // if (rc != 0) {
+        //     return switch (ws2_32.WSAGetLastError()) {
+        //         .WSAENOTSOCK => unreachable,
+        //         .WSAEINVAL => unreachable,
+        //         .WSAEFAULT => unreachable,
+        //         .WSAEWOULDBLOCK => unreachable,
+        //         else => |err| return windows.unexpectedWSAError(err),
+        //     };
+        // }
+    }
 }
 
 pub fn poll(self: *Self, timeout: i32) !void {
+    var events: [1024]pike.os.OVERLAPPED_ENTRY = undefined;
+
+    const num_events = try pike.os.GetQueuedCompletionStatusEx(self.handle, &events, @intCast(windows.DWORD, timeout), false);
+    for (events[0..num_events]) |e, i| {
+        const file = @intToPtr(*pike.File, e.lpCompletionKey);
+        file.trigger(.{ .read = true });
+    }
     // TODO(kenta): implement
 }
--- src/os.zig
+++ src/os.zig
@@ -4,12 +4,65 @@ const os = std.os;
 const windows = os.windows;
 const ws2_32 = windows.ws2_32;
 
+pub const OVERLAPPED_ENTRY = extern struct {
+    lpCompletionKey: windows.ULONG_PTR,
+    lpOverlapped: windows.LPOVERLAPPED,
+    Internal: windows.ULONG_PTR,
+    dwNumberOfBytesTransferred: windows.DWORD,
+};
+
 const funcs = struct {
+    extern "kernel32" fn GetQueuedCompletionStatusEx(
+        CompletionPort: windows.HANDLE,
+        lpCompletionPortEntries: [*]OVERLAPPED_ENTRY,
+        ulCount: windows.ULONG,
+        ulNumEntriesRemoved: *windows.ULONG,
+        dwMilliseconds: windows.DWORD,
+        fAlertable: windows.BOOL,
+    ) callconv(.Stdcall) windows.BOOL;
+
     extern "ws2_32" fn bind(s: ws2_32.SOCKET, addr: [*c]const std.os.sockaddr, namelen: std.os.socklen_t) callconv(.Stdcall) c_int;
     extern "ws2_32" fn listen(s: ws2_32.SOCKET, backlog: c_int) callconv(.Stdcall) c_int;
     extern "ws2_32" fn accept(s: ws2_32.SOCKET, addr: [*c]std.os.sockaddr, addrlen: [*c]std.os.socklen_t) callconv(.Stdcall) ws2_32.SOCKET;
     extern "ws2_32" fn setsockopt(s: ws2_32.SOCKET, level: c_int, optname: c_int, optval: [*c]const u8, optlen: c_int) callconv(.Stdcall) c_int;
 };
+
+pub const GetQueuedCompletionStatusError = error{
+    Aborted,
+    Cancelled,
+    EOF,
+    Timeout,
+} || os.UnexpectedError;
+
+pub fn GetQueuedCompletionStatusEx(
+    completion_port: windows.HANDLE,
+    completion_port_entries: []OVERLAPPED_ENTRY,
+    timeout_ms: ?windows.DWORD,
+    alertable: bool,
+) GetQueuedCompletionStatusError!u32 {
+    var num_entries_removed: u32 = 0;
+
+    const result = funcs.GetQueuedCompletionStatusEx(
+        completion_port,
+        completion_port_entries.ptr,
+        @intCast(windows.ULONG, completion_port_entries.len),
+        &num_entries_removed,
+        timeout_ms orelse windows.INFINITE,
+        @boolToInt(alertable),
+    );
+
+    if (result == windows.FALSE) {
+        return switch (windows.kernel32.GetLastError()) {
+            .ABANDONED_WAIT_0 => error.Aborted,
+            .OPERATION_ABORTED => error.Cancelled,
+            .HANDLE_EOF => error.EOF,
+            .IMEOUT => error.Timeout,
+            else => |err| windows.unexpectedError(err),
+        };
+    }
+
+    return num_entries_removed;
+}
 
 pub const SOL_SOCKET = if (builtin.os.tag == .windows) 0xffff else os.SOL_SOCKET;
 pub const SO_REUSEADDR = if (builtin.os.tag == .windows) 0x0004 else os.SO_REUSEADDR;
