4f3cb916754b09b64af47abd56eca714707e5fb6
lithdew <kenta@lithdew.net>
2020-11-11 23:50:07 +0900

    signal: add example

---
+++ example_signal.zig
@@ -0,0 +1,41 @@
+const std = @import("std");
+const pike = @import("pike.zig");
+
+const os = std.os;
+
+pub fn main() !void {
+    try pike.init();
+    defer pike.deinit();
+
+    const notifier = try pike.Notifier.init();
+    defer notifier.deinit();
+
+    var stopped = false;
+
+    var frame = async run(&notifier, &stopped);
+
+    while (!stopped) {
+        try notifier.poll(10_000);
+    }
+
+    try nosuspend await frame;
+}
+
+fn run(notifier: *const pike.Notifier, stopped: *bool) !void {
+    defer stopped.* = true;
+
+    var signal = try pike.Signal.init(.{ .interrupt = true });
+    defer signal.deinit();
+
+    std.debug.print("Press Ctrl+C.\n", .{});
+
+    try signal.wait();
+
+    std.debug.print("Do it again!\n", .{});
+
+    try signal.wait();
+
+    std.debug.print("I promise; one more time.\n", .{});
+
+    try signal.wait();
+}
---
+++ signal_linux.zig
@@ -0,0 +1,79 @@
+const std = @import("std");
+const pike = @import("pike.zig");
+const posix = @import("os/posix.zig");
+
+const os = std.os;
+const system = os.system;
+
+const mem = std.mem;
+
+usingnamespace @import("waker.zig");
+
+pub const SignalType = packed struct {
+    terminate: bool = false,
+    interrupt: bool = false,
+    quit: bool = false,
+    hup: bool = false,
+};
+
+pub const Signal = struct {
+    const Self = @This();
+
+    handle: pike.Handle,
+    prev: os.sigset_t,
+
+    lock: std.Mutex = .{},
+    readers: Waker = .{},
+
+    pub fn init(signal: SignalType) !Self {
+        var set = mem.zeroes(os.sigset_t);
+        if (signal.terminate) system.sigaddset(&set, os.SIGTERM);
+        if (signal.interrupt) system.sigaddset(&set, os.SIGINT);
+        if (signal.quit) system.sigaddset(&set, os.SIGQUIT);
+        if (signal.hup) system.sigaddset(&set, os.SIGHUP);
+
+        var prev = mem.zeroes(os.sigset_t);
+        try posix.sigprocmask(os.SIG_BLOCK, &set, &prev);
+
+        return Self{
+            .handle = .{
+                .inner = try os.signalfd(-1, &set, os.O_CLOEXEC),
+                .wake_fn = wake,
+            },
+            .prev = prev,
+        };
+    }
+
+    pub fn deinit(self: *const Self) void {
+        posix.sigprocmask(os.SIG_SETMASK, &self.prev, null) catch {};
+        os.close(self.handle.inner);
+    }
+
+    fn wake(handle: *pike.Handle, opts: pike.WakeOptions) void {
+        const self = @fieldParentPtr(Self, "handle", handle);
+        if (opts.read_ready) if (self.readers.wake(&self.lock)) |frame| resume frame;
+        if (opts.write_ready) @panic("pike/signal (linux): signalfd unexpectedly reported write-readiness");
+    }
+
+    pub fn wait(self: *Self) callconv(.Async) !void {
+        defer if (self.readers.next(&self.lock)) |frame| resume frame;
+
+        var info: os.signalfd_siginfo = undefined;
+
+        while (true) {
+            const num_bytes = os.read(self.handle.inner, mem.asBytes(&info)) catch |err| switch (err) {
+                error.WouldBlock => {
+                    self.readers.wait(&self.lock);
+                    continue;
+                },
+                else => return err,
+            };
+
+            if (num_bytes != @sizeOf(@TypeOf(info))) {
+                return error.ShortRead;
+            }
+
+            return;
+        }
+    }
+};
