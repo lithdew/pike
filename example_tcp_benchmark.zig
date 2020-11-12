b4ed8d7a4f0e6f2ffcc894a10e20f03d2eebd72a
lithdew <kenta@lithdew.net>
2020-11-13 02:10:27 +0900

    example/tcp_benchmark: make flag parsing more robust

--- example_tcp_benchmark.zig
+++ example_tcp_benchmark.zig
@@ -2,10 +2,22 @@ const std = @import("std");
 const pike = @import("pike.zig");
 
 const os = std.os;
+const fmt = std.fmt;
 const net = std.net;
 const mem = std.mem;
 const heap = std.heap;
 const process = std.process;
+
+fn exists(args: []const []const u8, flags: anytype) ?usize {
+    inline for (flags) |flag| {
+        for (args) |arg, index| {
+            if (mem.eql(u8, arg, flag)) {
+                return index;
+            }
+        }
+    }
+    return null;
+}
 
 pub fn main() !void {
     var gpa: heap.GeneralPurposeAllocator(.{}) = .{};
@@ -16,8 +28,25 @@ pub fn main() !void {
     const args = try process.argsAlloc(allocator);
     defer process.argsFree(allocator, args);
 
-    const run_server = args.len == 1 or (args.len > 1 and mem.eql(u8, args[1], "server"));
-    const run_client = args.len == 1 or (args.len > 1 and mem.eql(u8, args[1], "client"));
+    const run_server = args.len == 1 or (args.len > 1 and exists(args, .{ "server", "--server", "-server", "-s" }) != null);
+    const run_client = args.len == 1 or (args.len > 1 and exists(args, .{ "client", "--client", "-client", "-c" }) != null);
+
+    const address: net.Address = blk: {
+        const default = try net.Address.parseIp("127.0.0.1", 9000);
+
+        const index = exists(args, .{ "address", "--address", "-address", "-a" }) orelse {
+            break :blk default;
+        };
+
+        if (args.len <= index + 1) break :blk default;
+
+        var fields = mem.split(args[index + 1], ":");
+
+        const addr_host = fields.next().?;
+        const addr_port = try fmt.parseInt(u16, fields.next().?, 10);
+
+        break :blk try net.Address.parseIp(addr_host, addr_port);
+    };
 
     try pike.init();
     defer pike.deinit();
@@ -28,13 +57,13 @@ pub fn main() !void {
     var stopped = false;
 
     if (run_server) {
-        var server_frame = async runBenchmarkServer(&notifier, &stopped);
-        defer nosuspend await server_frame catch unreachable;
+        var server_frame = async runBenchmarkServer(&notifier, address, &stopped);
+        defer nosuspend await server_frame catch |err| @panic(@errorName(err));
     }
 
     if (run_client) {
-        var client_frame = async runBenchmarkClient(&notifier, &stopped);
-        defer nosuspend await client_frame catch unreachable;
+        var client_frame = async runBenchmarkClient(&notifier, address, &stopped);
+        defer nosuspend await client_frame catch |err| @panic(@errorName(err));
     }
 
     while (!stopped) {
@@ -42,10 +71,8 @@ pub fn main() !void {
     }
 }
 
-fn runBenchmarkServer(notifier: *const pike.Notifier, stopped: *bool) !void {
+fn runBenchmarkServer(notifier: *const pike.Notifier, address: net.Address, stopped: *bool) !void {
     defer stopped.* = true;
-
-    const address = try net.Address.parseIp("127.0.0.1", 9000);
 
     var socket = try pike.Socket.init(os.AF_INET, os.SOCK_STREAM, os.IPPROTO_TCP, 0);
     defer socket.deinit();
@@ -71,10 +98,8 @@ fn runBenchmarkServer(notifier: *const pike.Notifier, stopped: *bool) !void {
     }
 }
 
-fn runBenchmarkClient(notifier: *const pike.Notifier, stopped: *bool) !void {
+fn runBenchmarkClient(notifier: *const pike.Notifier, address: net.Address, stopped: *bool) !void {
     defer stopped.* = true;
-
-    const address = try net.Address.parseIp("127.0.0.1", 9000);
 
     var socket = try pike.Socket.init(os.AF_INET, os.SOCK_STREAM, os.IPPROTO_TCP, 0);
     defer socket.deinit();
